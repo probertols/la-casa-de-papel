7f3b6fca1313fbd46cb26019cdeb1d79bb973f18
Paulo Roberto <paulomd32@gmail.com>
2023-05-01 18:08:43 -0300

    exibindo taxas na tela

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
 const searchButton = document.querySelector('.search-btn');
 const coinInput = document.querySelector('#coin-input');
+const coinTitle = document.querySelector('.coin-title');
+const coinList = document.querySelector('.coin-list');
+
+const renderCoins = (coins) => {
+    coinList.innerHTML = '';
+    const coinArray = Object.entries(coins);
+    coinArray.map((coin) => {
+        const [coinName, value] = coin
+        const li = document.createElement('li');
+        li.innerHTML = `${coinName} - ${value}`;
+        coinList.append(li);
+
+    })
+}
 
 const handleSearch = () => {
   const coin = coinInput.value.toUpperCase(); 
   const url = `https://api.exchangerate.host/latest?base=${coin}`
- 
+  let rates;
+
   fetch(url)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    rates = data.rates;
+    renderCoins(data.rates);
+  });
+
+  coinTitle.innerHTML = `Valores referentes a 1 ${coin}`
    
+
 };
 
 searchButton.addEventListener('click', handleSearch);
