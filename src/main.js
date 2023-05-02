f2237dde761e8dd1ec3bb4ac07b71cb9a055228d
Paulo Roberto <paulomd32@gmail.com>
2023-05-01 21:40:04 -0300

    commit final - pronto

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 const searchButton = document.querySelector('.search-btn');
 const coinInput = document.querySelector('#coin-input');
 const coinTitle = document.querySelector('.coin-title');
@@ -9,7 +10,7 @@ const renderCoins = (coins) => {
     coinArray.map((coin) => {
         const [coinName, value] = coin
         const li = document.createElement('li');
-        li.innerHTML = `${coinName} - ${value}`;
+        li.innerHTML = `${coinName} - ${value.toFixed(2)}`;
         coinList.append(li);
 
     })
@@ -17,14 +18,33 @@ const renderCoins = (coins) => {
 
 const handleSearch = () => {
   const coin = coinInput.value.toUpperCase(); 
+  if(!coin){
+    Swal.fire({
+        icon: 'error',
+        title: 'Ops...',
+        text: 'Você precisa passar uma moeda!',
+    })
+    return;
+ }
+
   const url = `https://api.exchangerate.host/latest?base=${coin}`
   let rates;
 
   fetch(url)
   .then((response) => response.json())
   .then((data) => {
+    if(data.base !== coin){
+        throw new Error("Moeda não existente!")
+    }
     rates = data.rates;
     renderCoins(data.rates);
+  }).catch((err) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Ops...',
+        text: 'Moeda não existente',
+    });
+    coinTitle.innerHTML = '';
   });
 
   coinTitle.innerHTML = `Valores referentes a 1 ${coin}`
